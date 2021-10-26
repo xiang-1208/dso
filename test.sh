e4bd2d259844012a165988b56c03c86cff9036ae
xiang-1208 <1615582254@qq.com>
2021-10-26 13:11:49 +0800

    xiang

---
+++ test.sh
@@ -0,0 +1,8 @@
+bin/dso_dataset \
+	files=/media/xiang/Xiang_P/tum/sequence_01/images.zip \
+	calib=/media/xiang/Xiang_P/tum/sequence_01/camera.txt \
+	gamma=/media/xiang/Xiang_P/tum/sequence_01/pcalib.txt \
+	vignette=/media/xiang/Xiang_P/tum/sequence_01/vignette.png \
+	preset=0 \
+	mode=0
+
